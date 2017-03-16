adb2050d4da7e7cbeddf40710ec1a613aceac58a
Stefan Müller <s.mueller@it.kls-glt.de>
2017-03-16 15:15:36 +0100

    Zwischen Commit, leere good für nächste Aufgaben

---
+++ aufgaben/sheet04/sol1/good.rs
@@ -0,0 +1,17 @@
+fn main() {
+    
+}
+
+
+
+
+
+// Is it a happy number? https://en.wikipedia.org/wiki/Happy_number
+fn check_if_number_is_happy(number: i32) -> bool {
+    false
+}
+
+// is it prime?
+fn check_if_number_is_prime(n: i32) -> bool {
+    false
+}
